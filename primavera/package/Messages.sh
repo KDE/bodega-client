9200dfe65146099a475888f107c053b72e377990
Albert Astals Cid <aacid@kde.org>
2013-04-06 15:23:33 +0200

    Generate a pot file
    CCMAIL: aseigo@kde.org

--- primavera/package/Messages.sh
+++ primavera/package/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_package_com.makeplaylive.primavera
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_package_com.makeplaylive.primavera.pot
 rm -f rc.cpp
 
