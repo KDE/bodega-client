cc9315217a09cae8a46557e30f2cd96ed4af6602
Aaron Seigo <aseigo@kde.org>
2013-08-08 12:25:59 +0200

    the cast is unecessary

--- lib/bodega/assetoperations.cpp
+++ lib/bodega/assetoperations.cpp
@@ -218,7 +218,7 @@ void AssetOperations::launch()
 
 QAbstractItemModel *AssetOperations::ratingsModel()
 {
-    return qobject_cast<QAbstractItemModel*>(d->ratingsModel);
+    return d->ratingsModel;
 }
 
 }
